52a61a546ce0c65930366d16591f52f4da1d01c9
kavehkm <mehrbaniankaveh@gmail.com>
2019-04-07 11:15:26 +0430

    create routes from myshop.ursl--->shop.urls to shop.views

---
+++ myshop/shop/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from shop import views
+
+
+
+urlpatterns = [
+    path('', views.book_list, name='book_list'),
+    path('category/<slug:category_slug>/books/', views.book_list, name='books_by_category'),
+    path('book/<int:book_id>/<slug:book_slug>/', views.book_detail, name='book_detail'),
+]
