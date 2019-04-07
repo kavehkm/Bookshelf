c41d8e2caaf7fdca23d5d8bab41a213e6e9952ad
kavehkm <mehrbaniankaveh@gmail.com>
2019-04-07 11:11:01 +0430

    register shop models import in django admin

--- myshop/shop/admin.py
+++ myshop/shop/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from shop.models import Category, Book
 
-# Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'category', 'image', 'description', 'price', 'stock', 'available', 'created', 'update']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ['available', 'created', 'update', 'category']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Book, BookAdmin)
