951ace71c3391b7d3c9d09b819862009f90a6183
kavehkm <mehrbaniankaveh@gmail.com>
2019-04-07 15:33:35 +0430

    create views for cart app(add/remove item to/from Cart, Cart detail)

--- myshop/cart/views.py
+++ myshop/cart/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from shop.models import Category, Book
+from cart.cart import Cart
+from  cart.forms import CartAddBookForm
 
-# Create your views here.
+
+
+@require_POST
+def cart_add(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    cart = Cart(request)
+    form = CartAddBookForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        cart.add(book=book, quantity=cd['quantity'], update_quantity=cd['update'])
+    return redirect('cart_detail')
+
+
+def cart_remove(request, book_id):
+    cart = Cart(request)
+    book = get_object_or_404(Book, id=book_id)
+    cart.remove(book)
+    return redirect('cart_detail')
+
+
+def cart_detail(request):
+    cart = Cart(request)
+    for item in cart:
+        item['update_quantity_form'] = CartAddBookForm(
+            initial={
+                'quantity': item['quantity'],
+                'update': True
+            }
+        )
+    context = {'cart': cart}
+    return render(request, 'cart/cart_detail.html', context)
+
