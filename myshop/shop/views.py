e418950b52a0726f6d1638de2bd427b9f2b0b8b0
kavehkm <mehrbaniankaveh@gmail.com>
2019-04-07 11:14:08 +0430

    create views for shop app (book_list, book_detail)

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from shop.models import Category, Book
 
-# Create your views here.
+
+
+def book_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    books = Book.objects.filter(available=True).order_by('-created')
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        books = books.filter(category=category)
+    context = {
+        'category': category,
+        'categories': categories,
+        'books': books
+    }
+    return render(request, 'shop/book_list.html', context)
+
+
+def book_detail(request, book_id, book_slug):
+    book = get_object_or_404(Book, id=book_id, slug=book_slug, available=True)
+    context = {'book': book}
+    return render(request, 'shop/book_detail.html', context)
