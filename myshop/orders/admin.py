ea9844975161ee11959a52872653e1526948a791
kavehkm <mehrbaniankaveh@gmail.com>
2019-04-09 11:08:50 +0430

    register orders app models in admin site

---
+++ myshop/orders/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from orders.models import Order, OrderItem
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['book']
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email', 'address', 'province', 'city', 'created', 'update', 'paid']
+    list_filter = ['paid', 'created', 'update']
+    inlines = [OrderItemInline]
+
+
+
+admin.site.register(Order, OrderAdmin)
