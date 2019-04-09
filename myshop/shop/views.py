79b93ac059c6459df027f7f8f0c9b9b0544c3a59
kavehkm <mehrbaniankaveh@gmail.com>
2019-04-09 11:15:34 +0430

    add update form for book_detail function

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from shop.models import Category, Book
+from cart.forms import CartAddBookForm
 
 
 
@@ -7,18 +8,24 @@ def book_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     books = Book.objects.filter(available=True).order_by('-created')
+    books_count = books.count()
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         books = books.filter(category=category)
     context = {
         'category': category,
         'categories': categories,
-        'books': books
+        'books': books,
+        'books_count': books_count
     }
     return render(request, 'shop/book_list.html', context)
 
 
 def book_detail(request, book_id, book_slug):
     book = get_object_or_404(Book, id=book_id, slug=book_slug, available=True)
-    context = {'book': book}
+    form = CartAddBookForm()
+    context = {
+        'book': book,
+        'form': form
+    }
     return render(request, 'shop/book_detail.html', context)
